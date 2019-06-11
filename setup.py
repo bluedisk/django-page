14ef0951de11961e1f3716db266976fed3779283
bluedisk <bluedisk@gmail.com>
2019-06-11 20:11:00 +0900

    0.0.6 - popup for mobile

--- setup.py
+++ setup.py
@@ -23,10 +23,11 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.4',
+    version='0.0.6',
 
     description='Django korean style simplest CMS',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/bluedisk/django-page',
